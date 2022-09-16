ba75cd5014d8f30914e9911722042584b5ec37f8
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-16 03:12:43 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,9 @@
+// # Exercise 3
+
+// * Create 3 variables: `area`, `x`, `y`
+// * Use the variable called `area` to print in console the area of the rectangle.
+
+let y = 4;
+let x = 3;
+const area = x * y;
+console.log(area);
